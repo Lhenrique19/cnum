f86086f59faad84f0d4ab3b208e9665984193298
Lhenrique19 <111003205+Lhenrique19@users.noreply.github.com>
2022-10-19 16:45:21 +0000

    Derivadas numéricas(Regressiva, Progressiva e central)

--- Aula 11/main.jl
+++ Aula 11/main.jl
@@ -27,18 +27,58 @@ println(r)
 
 println("\nExercício 8.1.1")
 
+println("\nF(x) = sen(x)\n")
+
 f(x) = sin(x)
 
+
+r = dp(f, 2.0, 10^-2)
+println(r)
+r = dp(f, 2.0, 10^-3)
+println(r)
+
+println("\n")
 
 r = dr(f, 2.0, 10^-2)
 println(r)
 r = dr(f, 2.0, 10^-3)
 println(r)
 
+println("\n")
+
+r = dc(f, 2.0, 10^-2)
+println(r)
+r = dc(f, 2.0, 10^-3)
+println(r)
+
+println("\n")
+r = derivative(f, 2.0)
+println(r)
+
+println("\n")
+
+println("\nF(x) = e^-x\n")
 f(x) = MathConstants.e^(-x)
+
+r = dp(f, 1.0, 10^-2)
+println(r)
+r = dp(f, 1.0, 10^-3)
+println(r)
+
+println("\n")
 
 r = dr(f, 1.0, 10^-2)
 println(r)
 r = dr(f, 1.0, 10^-3)
 println(r)
 
+println("\n")
+
+r = dc(f, 1.0, 10^-2)
+println(r)
+r = dc(f, 1.0, 10^-3)
+println(r)
+
+println("\n")
+r = derivative(f, 1.0)
+println(r)
